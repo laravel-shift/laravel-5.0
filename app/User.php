8fc445cdb694a4a783537eb35e5c5062a0322523
Laravel Shift <shift@laravelshift.com>
2016-05-02 18:45:12 -0400

    Shift Authorization
    Laravel 5.1.11 introduces [Authorization][1]. This updates the app to
    use the new contracts, facades, traits, and providers.
    
    [1]: http://laravel.com/docs/5.1/authorization

--- app/User.php
+++ app/User.php
@@ -6,10 +6,13 @@ use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
 {
 
-    use Authenticatable, CanResetPassword;
+    use Authenticatable, Authorizable, CanResetPassword;
 
     /**
      * The database table used by the model.
