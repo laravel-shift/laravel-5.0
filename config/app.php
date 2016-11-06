3e6b62ffd5c49c61ee8b9ceb7c6c5389457beb00
Laravel Shift <shift@laravelshift.com>
2016-11-06 18:01:25 +0000

    Remove old providers
    Laravel 5.1 removes the `BusServiceProvider` and
    `ConfigServiceProvider` providers.

--- config/app.php
+++ config/app.php
@@ -141,8 +141,6 @@ return [
 		 * Application Service Providers...
 		 */
         'App\Providers\AppServiceProvider',
-        'App\Providers\BusServiceProvider',
-        'App\Providers\ConfigServiceProvider',
         'App\Providers\EventServiceProvider',
         'App\Providers\RouteServiceProvider',
 
